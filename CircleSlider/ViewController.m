f7f575cc5f9db0969df5dfe562379c3383af896e
softgoto <softgoto@gmail.com>
2015-06-12 18:35:35 +0800

    Initial Commit

---
+++ CircleSlider/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CircleSlider
+//
+//  Created by xuhui on 15/6/12.
+//  Copyright (c) 2015年 xuhui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
